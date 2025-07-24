2f84203ca36abc4b8d42281f708e6587a82935ab
GrayJack <gr41.j4ck@gmail.com>
2025-07-24 17:03:19 -0300

    feat(popcorn): Implement loading POPS Compatibility flags configuration

--- cef/popcorn/main.c
+++ cef/popcorn/main.c
@@ -32,6 +32,15 @@ static u8 pgd_buf[0x80];
 // If the launched software is official (0 = CUSTOM, 1 = OFFICIAL)
 static int is_official = 0;
 
+// Custom emulator config
+static u8 custom_config[0x400];
+// Custom emulator config size;
+static int config_size = 0;
+// pops supports up to 5 discs, but config is the same for all of them even if
+// it doesn't have to
+static int psiso_offsets[5] = {0, 0, 0, 0, 0};
+
+
 STMOD_HANDLER previous;
 
 int scePopsManExitVSHKernelPatched(u32 destSize, u8 *src, u8 *dest) {
@@ -87,8 +96,41 @@ int sceIoIoctlPatched(SceUID fd, unsigned int cmd, void *indata, int inlen, void
 	return 0;
 }
 
-int sceIoReadPatched(SceUID fd, void *data, SceSize size) {
+int sceIoReadPatched(SceUID fd, u8 *data, SceSize size) {
+	u32 k1 = pspSdkSetK1(0);
+	u32 pos = sceIoLseek32(fd, 0, SEEK_CUR);
 	int res = sceIoRead(fd, data, size);
+
+	// Inject custom config
+	for (int i = 0; i < NELEMS(psiso_offsets); i++) {
+		if (psiso_offsets[i] == 0) {
+			break;
+		}
+
+		// Emulator reads a huge chunk of data starting at PSISOIMG+0x400
+		// More information about PSISOIMG: https://www.psdevwiki.com/psp/PSISOIMG0000
+		u32 huge_chunk_read_pos = psiso_offsets[i]+0x400;
+
+		// If not the read we expect, go to the next possibility
+		if (pos != huge_chunk_read_pos) {
+			continue;
+		}
+
+		// Seek to where PSISOIMG magic is expected to appear and read it
+		char magic[12];
+		sceIoLseek(fd, psiso_offsets[i], PSP_SEEK_SET);
+		sceIoRead(fd, magic, sizeof(magic));
+		// Seek back into where file cursor should be.
+		sceIoLseek(fd, pos+size, PSP_SEEK_SET);
+
+		// Magic is correct AND custom config exists
+		if (memcmp(magic, "PSISOIMG0000", 12) == 0 && config_size > 0) {
+			// Copy custom config if it exists
+			// It is located at 0x420 after PSISOIMG, thus 0x20 after given buffer
+			memcpy(data+0x20, custom_config, config_size);
+			logmsg2("%s: Custom config was set.\n", __func__);
+		}
+	}
 
 	// Fake ~PSP magic to avoid crash
 	if (size == sizeof(u32)) {
@@ -96,11 +138,13 @@ int sceIoReadPatched(SceUID fd, void *data, SceSize size) {
 		if (memcmp(data, &magic, sizeof(u32)) == 0) {
 			magic = PSP_MAGIC;
 			memcpy(data, &magic, sizeof(u32));
-			logmsg3("%s: Faked PSP magic\n");
+			logmsg3("%s: [FAKE] PSP magic\n", __func__);
 		}
 	}
 
-	logmsg2("%s: fd=0x%08X, data=0x%08X, size=%d -> 0x%08X\n", __func__, fd, data, size, res);
+exit:
+	pspSdkSetK1(k1);
+	logmsg3("%s: fd=0x%08X, data=0x%08X, size=0x%08X -> 0x%08X\n", __func__, fd, data, size, res);
 	return res;
 }
 
@@ -189,6 +233,141 @@ static void setPsxCompiledFwVersion(u32 version) {
 
 }
 
+/// Init `is_official`, `psiso_offsets`, `pgd_buf`, `custom_config`, `config_size`
+static int initGlobals() {
+	const char * filename = sceKernelInitFileName();
+
+	if (NULL == filename) {
+		return -1;
+	}
+
+	SceUID fd = sceIoOpen(filename, PSP_O_RDONLY, 0);
+	if (fd < 0) {
+		logmsg("%s: [ERROR]: sceIoOpen %s -> 0x%08X\n", __func__, filename, fd);
+		return fd;
+	}
+
+	// Read header
+	PBPHeader header;
+	int io_ret = sceIoRead(fd, &header, sizeof(PBPHeader));
+
+	if (io_ret < 0) {
+		logmsg("%s: [ERROR]: sceIoRead PBP header -> 0x%08X\n", __func__, io_ret);
+		sceIoClose(fd);
+		return io_ret;
+	}
+
+	// Get magic
+	char magic[16];
+	sceIoLseek(fd, header.psar_offset, PSP_SEEK_SET);
+	io_ret = sceIoRead(fd, magic, sizeof(magic));
+
+	if (io_ret < 0) {
+		logmsg("%s: [ERROR]: sceIoRead PSISOIMG magic -> 0x%08X\n", __func__, io_ret);
+		sceIoClose(fd);
+		return io_ret;
+	}
+
+	// Reset psiso_offset
+	memset(psiso_offsets, 0, sizeof(psiso_offsets));
+
+	if (memcmp(magic, "PSISOIMG0000", 12) == 0) { // Single-Disc
+		// Start at psar_offset
+		psiso_offsets[0] = header.psar_offset;
+
+		// Prepare for reading PGD
+		sceIoLseek(fd, header.psar_offset + 0x400, PSP_SEEK_SET);
+	} else if (memcmp(magic, "PSTITLEIMG000000", 16) == 0) { // Multi-Disc
+		// Multi disc, offsets are stored at psar+0x200
+		sceIoLseek(fd, header.psar_offset + 0x200, PSP_SEEK_SET);
+		sceIoRead(fd, psiso_offsets, sizeof(psiso_offsets));
+
+		// Adjust to make offsets absolute.
+		for (int i = 0; i < NELEMS(psiso_offsets) && psiso_offsets[i] != 0; i++) {
+			// Offsets are relative to psar
+			psiso_offsets[i] += header.psar_offset;
+		}
+
+		// Prepare for reading PGD
+		sceIoLseek(fd, header.psar_offset + 0x200, PSP_SEEK_SET);
+	} else {
+		sceIoClose(fd);
+		logmsg("%s: [ERROR]: Failed to find PSISOIMG magic\n", __func__);
+		return -1;
+	}
+
+	// Read PGD buffer
+	io_ret = sceIoRead(fd, pgd_buf, sizeof(pgd_buf));
+
+	if (io_ret < 0) {
+		logmsg("%s: [ERROR]: sceIoRead PGD -> 0x%08X\n", __func__, io_ret);
+		sceIoClose(fd);
+		return io_ret;
+	}
+	logmsg3("%s: `pgd_buf` set\n", __func__);
+
+	// Close fd
+	sceIoClose(fd);
+
+	// Must have at least one disc.
+	if (psiso_offsets[0] == 0) {
+		logmsg("%s: [ERROR]: Zero discs\n", __func__);
+		return -1;
+	}
+	logmsg3("%s: `psiso_offsets` set\n", __func__);
+
+	// Check PGD magic
+	if (((u32 *)pgd_buf)[0] == PGD_MAGIC) {
+		is_official = 1;
+	} else {
+		is_official = 0;
+	}
+	logmsg3("%s: `is_official` set\n", __func__);
+
+	// Check and read config.bin
+	//
+	// From now on, errors should be ignored since custom config are optional.
+	// 1. Set filename
+	char config_filename[256] = {0};
+	strcpy(config_filename, filename);
+	char* slash = strrchr(config_filename, '/');
+	if (!slash) {
+		logmsg("%s: [ERROR]: Ignoring custom config: Invalid filename to find custom config: %s\n", __func__, filename);
+	}
+	strcpy(slash+1, "CONFIG.BIN");
+
+	// 2. Open file
+	fd = -1;
+	fd = sceIoOpen(config_filename, PSP_O_RDONLY, 0777);
+	if (fd < 0) {
+		logmsg("%s: [ERROR]: sceIoOpen %s -> 0x%08X\n", __func__, config_filename, fd);
+	}
+
+	// 3. Read it if it exists
+	if (fd > 0) {
+		config_size = sceIoLseek(fd, 0, PSP_SEEK_END);
+		if (config_size <= 0) {
+			logmsg("%s: [ERROR]: Ignoring custom config: Fail to get custom config size\n", __func__);
+			sceIoClose(fd);
+			return 0;
+		}
+
+		sceIoLseek(fd, 0, PSP_SEEK_SET);
+		io_ret = sceIoRead(fd, custom_config, config_size);
+
+		if (io_ret < 0) {
+			logmsg("%s: [ERROR]: Ignoring custom config: Fail to read custom config size\n", __func__);
+			sceIoClose(fd);
+			return 0;
+		}
+
+		logmsg3("%s: `custom_config` set: 0x%08X bytes\n", __func__, config_size);
+		sceIoClose(fd);
+	}
+
+	return 0;
+}
+
 static void patchScePopsMgr(void) {
 	SceModule2 *mod = sceKernelFindModuleByName("scePops_Manager");
 	u32 text_addr = mod->text_addr;
@@ -210,11 +389,13 @@ static void patchScePopsMgr(void) {
 	_sceMeAudio_2AB4FE43 = (void*)sctrlHENFindFunctionInMod(mod, "sceMeAudio", 0x2AB4FE43);
 	sctrlHENHookImportByNID(mod, "sceMeAudio", 0x2AB4FE43, sceMeAudio_2AB4FE43_Patched, 1);
 
+	sctrlHENHookImportByNID(mod, "IoFileMgrForKernel", 0x6A638D83, sceIoReadPatched, 0);
+
 	if (!is_official) {
 		// Fake dnas drm
 		sctrlHENHookImportByNID(mod, "IoFileMgrForKernel", 0x109F50BC, sceIoOpenPatched, 0);
 		sctrlHENHookImportByNID(mod, "IoFileMgrForKernel", 0x63632449, sceIoIoctlPatched, 0);
-		sctrlHENHookImportByNID(mod, "IoFileMgrForKernel", 0x6A638D83, sceIoReadPatched, 0);
+
 
 		// Dummying amctrl decryption functions
 		MAKE_DUMMY_FUNCTION(text_addr + 0xA90, 1);
@@ -259,38 +440,9 @@ int module_start(SceSize args, void *argp) {
 	logInit("ms0:/log_popcorn.txt");
 	logmsg("Popcorn started...\n");
 
-	SceUID fd = sceIoOpen(sceKernelInitFileName(), PSP_O_RDONLY, 0);
-	if (fd < 0) {
-		return fd;
-	}
-
-	// Read header
-	PBPHeader header;
-	sceIoRead(fd, &header, sizeof(PBPHeader));
-
-	// Get magic
-	char magic[16];
-	sceIoLseek(fd, header.psar_offset, PSP_SEEK_SET);
-	sceIoRead(fd, magic, sizeof(magic));
-
-	if (memcmp(magic, "PSISOIMG0000", 12) == 0) { // Single-Disc
-		sceIoLseek(fd, header.psar_offset + 0x400, PSP_SEEK_SET);
-	} else if (memcmp(magic, "PSTITLEIMG000000", 16) == 0) { // Multi-Disc
-		sceIoLseek(fd, header.psar_offset + 0x200, PSP_SEEK_SET);
-	} else {
-		sceIoClose(fd);
-		return 1;
-	}
-
-	// Read PGD buffer
-	sceIoRead(fd, pgd_buf, sizeof(pgd_buf));
-
-	// Close fd
-	sceIoClose(fd);
-
-	// Check PGD magic
-	if (((u32 *)pgd_buf)[0] == PGD_MAGIC) {
-		is_official = 1;
+	int res = initGlobals();
+	if (res < 0) {
+		return res;
 	}
 
 	setPsxCompiledFwVersion(0x06060110);
