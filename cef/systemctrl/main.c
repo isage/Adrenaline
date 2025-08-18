a4015ee7cd3a7e97a4be84d7c0a485db682663d0
GrayJack <gr41.j4ck@gmail.com>
2025-08-18 02:01:30 -0300

    feat(systemctrl): Add game patches related to Inferno ISO ca

--- cef/systemctrl/main.c
+++ cef/systemctrl/main.c
@@ -344,16 +344,16 @@ static int utilityGetParamPatched_ULJM05221(int param, int* value) {
 }
 
 static int wweModuleOnStart(SceModule2 * mod) {
-    // Boot Complete Action not done yet
-    if (strcmp(mod->modname, "mainPSP") == 0) {
-        sctrlHENHookImportByNID(mod, "scePower", 0x34F9C463, NULL, 222); // scePowerGetPllClockFrequencyInt
-        sctrlHENHookImportByNID(mod, "scePower", 0x843FBF43, NULL, 0);   // scePowerSetCpuClockFrequency
-        sctrlHENHookImportByNID(mod, "scePower", 0xFDB5BFE9, NULL, 222); // scePowerGetCpuClockFrequencyInt
-        sctrlHENHookImportByNID(mod, "scePower", 0xBD681969, NULL, 111); // scePowerGetBusClockFrequencyInt
-    }
+	// Boot Complete Action not done yet
+	if (strcmp(mod->modname, "mainPSP") == 0) {
+		sctrlHENHookImportByNID(mod, "scePower", 0x34F9C463, NULL, 222); // scePowerGetPllClockFrequencyInt
+		sctrlHENHookImportByNID(mod, "scePower", 0x843FBF43, NULL, 0);   // scePowerSetCpuClockFrequency
+		sctrlHENHookImportByNID(mod, "scePower", 0xFDB5BFE9, NULL, 222); // scePowerGetCpuClockFrequencyInt
+		sctrlHENHookImportByNID(mod, "scePower", 0xBD681969, NULL, 111); // scePowerGetBusClockFrequencyInt
+	}
 
-    // Call Previous Module Start Handler
-    if(game_previous) game_previous(mod);
+	// Call Previous Module Start Handler
+	if(game_previous) game_previous(mod);
 
 	return 0;
 }
@@ -366,10 +366,21 @@ static void PatchGameByGameId() {
 		utilityGetParam = (void*)FindProc("sceUtility_Driver", "sceUtility", 0xA5DA2406);
 		sctrlHENPatchSyscall((u32)utilityGetParam, utilityGetParamPatched_ULJM05221);
 
-	} else if (strcasecmp("ULES01472", game_id) == 0 || strcasecmp("ULUS10543", game_id) == 0){
-        // Patch Smakdown vs RAW 2011 anti-CFW check (CPU speed)
+	} else if (strcasecmp("ULES01472", game_id) == 0 || strcasecmp("ULUS10543", game_id) == 0) {
+		// Patch Smakdown vs RAW 2011 anti-CFW check (CPU speed)
 		game_previous = sctrlHENSetStartModuleHandler(wweModuleOnStart);
-    }
+
+	} else if (strcasecmp("ULES00590", game_id) == 0 || strcasecmp("ULJM05075", game_id) == 0) {
+		// Patch Aces of War anti-CFW check (UMD speed)
+		if (config.umd_seek == 0 && config.umd_speed == 0) {
+			void (*SetUmdDelay)(int, int) = (void*)sctrlHENFindFunction("EPI-InfernoDriver", "inferno_driver", 0xB6522E93);
+			if (SetUmdDelay != NULL) {
+				SetUmdDelay(1, 1);
+			}
+
+		}
+
+	}
 }
 
 static void PatchGamesByMod(SceModule2* mod) {
@@ -377,6 +388,22 @@ static void PatchGamesByMod(SceModule2* mod) {
 
 	if (strcmp(modname, "DJMAX") == 0 || strcmp(modname, "djmax") == 0) {
 		sctrlHENHookImportByNID(mod, "IoFileMgrForUser", 0xE3EB004C, NULL, 0);
+
+		if (config.iso_cache != CACHE_CONFIG_OFF) {
+			SceModule2* inferno_mod = sceKernelFindModuleByName("EPI-InfernoDriver");
+
+			// enable UMD reading speed
+			void (*SetUmdDelay)(int, int) = (void*)sctrlHENFindFunctionInMod(inferno_mod, "inferno_driver", 0xB6522E93);
+			if (SetUmdDelay != NULL) {
+				SetUmdDelay(2, 2);
+			}
+
+			// Disable Inferno cache
+			int (*CacheInit)(int, int, int) = (void*)sctrlHENFindFunctionInMod(inferno_mod, "inferno_driver", 0x8CDE7F95);
+			if (CacheInit != NULL) {
+				CacheInit(0, 0, 0);
+			}
+		}
 
 	} else if (strcmp(mod->modname, "ATVPRO") == 0){
 		// Remove "overclock" message in ATV PRO
@@ -439,7 +466,7 @@ static void OnSystemStatusIdle() {
 			}
 
 			int (*CacheSetPolicy)(int) = (void*)sctrlHENFindFunctionInMod(inferno_mod, "inferno_driver", 0xC0736FD6);
-			if (CacheSetPolicy) {
+			if (CacheSetPolicy != NULL) {
 				if (config.iso_cache == CACHE_CONFIG_LRU) {
 					CacheSetPolicy(CACHE_POLICY_LRU);
 					logmsg3("[INFO]: Inferno ISO cache policy: LRU\n");
