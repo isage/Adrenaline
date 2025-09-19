86959a861ae73a29d9747dbdbd60f509185aadb5
GrayJack <gr41.j4ck@gmail.com>
2025-09-19 00:49:38 -0300

    refactor(xmbctrl): Remove unused function

--- cef/xmbctrl/patch_vsh.c
+++ cef/xmbctrl/patch_vsh.c
@@ -87,48 +87,6 @@ static void (*OnRetry)() = NULL;
 static void (*AddSysconfItem)(u32 *option, SceSysconfItem **item) = NULL;
 static void (*OnInitMenuPspConfig)() = NULL;
 
-static SceOff findPkgOffset(const char *filename, unsigned *size, const char *pkgpath) {
-	int pkg = sceIoOpen(pkgpath, PSP_O_RDONLY, 0777);
-	if (pkg < 0)
-		return 0;
-
-	unsigned pkgsize = sceIoLseek32(pkg, 0, PSP_SEEK_END);
-	unsigned size2 = 0;
-
-	sceIoLseek32(pkg, 0, PSP_SEEK_SET);
-
-	if (size != NULL)
-		*size = 0;
-
-	unsigned offset = 0;
-	char name[64];
-
-	while (offset != 0xFFFFFFFF) {
-		sceIoRead(pkg, &offset, 4);
-		if (offset == 0xFFFFFFFF) {
-			sceIoClose(pkg);
-			return 0;
-		}
-		unsigned namelength;
-		sceIoRead(pkg, &namelength, 4);
-		sceIoRead(pkg, name, namelength + 1);
-
-		if (!strncmp(name, filename, namelength)) {
-			sceIoRead(pkg, &size2, 4);
-
-			if (size2 == 0xFFFFFFFF)
-				size2 = pkgsize;
-
-			if (size != NULL)
-				*size = size2 - offset;
-
-			sceIoClose(pkg);
-			return offset;
-		}
-	}
-	return 0;
-}
-
 static void *addCustomVshItem(int id, char *text, int action_arg, SceVshItem *orig) {
 	SceVshItem *item = (SceVshItem *)sce_paf_private_malloc(sizeof(SceVshItem));
 	sce_paf_private_memcpy(item, orig, sizeof(SceVshItem));
