cc4c14b4ad97e75c12efed348a76f28e27d9e28b
GrayJack <gr41.j4ck@gmail.com>
2026-01-12 17:26:59 -0300

    fix(systemctrl): Fix PaRappa the Rapper crash on `Force High Memory Layout` set to stable

--- cef/systemctrl/main.c
+++ cef/systemctrl/main.c
@@ -250,7 +250,7 @@ static int OnModuleStart(SceModule *mod) {
 
 		if (sceKernelApplicationType() == SCE_APPTYPE_GAME  && config.force_high_memory != HIGHMEM_OPT_OFF) {
 			if (config.force_high_memory == HIGHMEM_OPT_STABLE) {
-				sctrlHENSetMemory(40, 9);
+				sctrlHENSetMemory(40, 8);
 			} else if (config.force_high_memory == HIGHMEM_OPT_MAX) {
 				sctrlHENSetMemory(52, 0);
 			}
