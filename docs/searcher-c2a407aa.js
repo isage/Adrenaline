608eb17ea5635d2037efff3e22b8b16c53ab8c6b
GrayJack <gr41.j4ck@gmail.com>
2026-01-19 07:26:18 -0300

    docs: Update

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d644f00e.js',
+                path_to_root + 'searchindex-1f1a5430.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
