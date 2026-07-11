98ef8d623018e9977c6c8dd222137c436fcf60fa
GrayJack <gr41.j4ck@gmail.com>
2026-07-11 20:51:32 -0300

    v8.0.2

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-509df70e.js',
+                path_to_root + 'searchindex-8306d78f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
